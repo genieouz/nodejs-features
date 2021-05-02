6dc211b67001272f491c73a2eeda894f3e646e15
Ousmane Sakho <genieouzog@gmail.com>
2021-05-02 16:03:10 +0000

    simple copy and copy with stream

---
+++ copy.js
@@ -0,0 +1,32 @@
+var fs = require("fs");
+const copy = (path) => {
+    console.log("Copy in progress.... ", path);
+    fs.readFile(path, (error, file) => {
+        if(error) throw error
+        fs.writeFile("myfile-copy.mp3", file, (err, file) => {
+            if (err) throw err
+            console.log("Copy completed!");
+        });
+    });
+}
+
+const copyWithStream = (path) => {
+    console.log("Copy in progress.... ", path);
+    fs.stat(path, (err, stat) => {
+        if(err) throw err
+        const fileSize = stat.size;
+        let bytesRead = 0;
+        const read = fs.createReadStream(path);
+        const write = fs.createWriteStream("myfile-copie.mp4");
+        read.on("data", chunk => {
+            bytesRead += chunk.length;
+            console.log(`progress: ${((bytesRead/fileSize)*100).toFixed(2)}%`);
+        });
+        read.pipe(write);
+        read.on("end", () => {
+            console.log("Copy completed!");
+        });
+    })
+}
+
+module.exports = { copy, copyWithStream }
---
+++ index.js
@@ -0,0 +1,4 @@
+var copy = require("./copy").copy;
+var copyWithStream = require("./copy").copyWithStream;
+// copy("myfile.mp4");
+copyWithStream("myfile.mp4");
